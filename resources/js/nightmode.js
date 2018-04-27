70ecc3bcddcf8906b13c99de2edd8966e5e2b708
Benjamin Cooper <bencooper222@gmail.com>
2018-04-27 17:40:07 -0500

    Minimally works during day

--- resources/js/nightmode.js
+++ resources/js/nightmode.js
@@ -1,19 +1,11 @@
 // adjust day night thing
 import SunCalc from 'suncalc';
-//import * as moment from 'moment-timezone';
+import { DateTime } from 'luxon';
+
+const ENABLE_CACHE = true;
 
 const changeBackground = document.getElementsByTagName('body');
 const changeColor = colorToChangeElements();
-
-function colorToChangeElements() {
-  let icons = document.getElementsByTagName('i');
-  let svgs = document.getElementsByClassName('not-fa');
-
-  let h2 = document.getElementsByTagName('h2');
-  let h3 = document.getElementsByTagName('h3');
-
-  return addArrays([icons, h2, h3, svgs]);
-}
 
 function addArrays(arrays) {
   // arrays is an array of arrays
@@ -26,18 +18,27 @@ function addArrays(arrays) {
   return rtn;
 }
 
-function makeDay() {
+function colorToChangeElements() {
+  let icons = document.getElementsByTagName('i');
+  let svgs = document.getElementsByClassName('not-fa');
+
+  let h2 = document.getElementsByTagName('h2');
+  let h3 = document.getElementsByTagName('h3');
+
+  return addArrays([icons, h2, h3, svgs]);
+}
+
+const makeDay = () => {
   for (let i = 0; i < changeBackground.length; i++) {
-    console.log();
     changeBackground[i].style.backgroundColor = 'white';
   }
 
   for (let i = 0; i < changeColor.length; i++) {
     changeColor[i].style.color = 'black';
   }
-}
+};
 
-function makeNight() {
+const makeNight = () => {
   for (let i = 0; i < changeBackground.length; i++) {
     console.log();
     changeBackground[i].style.backgroundColor = 'black';
@@ -46,9 +47,9 @@ function makeNight() {
   for (let i = 0; i < changeColor.length; i++) {
     changeColor[i].style.color = 'white';
   }
-}
+};
 
-function getSunriseSunsetTimes() {
+const getSunriseSunsetTimes = () => {
   return fetch('https://freegeoip.net/json/')
     .then(location => {
       return location.json();
@@ -59,11 +60,13 @@ function getSunriseSunsetTimes() {
         coords.latitude,
         coords.longitude,
       );
+
       return { start: sunTimes.dawn, stop: sunTimes.sunset };
     })
     .then(data => {
-      let civilBegin = moment.tz(data.start, moment.tz.guess());
-      let civilEnd = moment.tz(data.stop, moment.tz.guess());
+      // const format = '';
+      let civilBegin = DateTime.fromISO(data.start.toISOString());
+      let civilEnd = DateTime.fromISO(data.stop.toISOString());
 
       let civilTimes = {
         begin: civilBegin,
@@ -72,38 +75,40 @@ function getSunriseSunsetTimes() {
 
       return civilTimes;
     });
-}
+};
 
-function calculateCorrectState() {
+const calculateCorrectState = () => {
   getSunriseSunsetTimes().then(function(data) {
-    let now = moment();
-    if (now.isAfter(data.begin) && now.isBefore(data.end)) {
+    let now = DateTime.local();
+    if (now > data.begin && now < data.end) {
       console.log(
         'It is between: ' +
-          data.begin.format('MMMM Do YYYY, h:mm:ss a') +
+          data.begin.toLocaleString(DateTime.DATETIME_FULL) +
           ' and ' +
-          data.end.format('MMMM Do YYYY, h:mm:ss a'),
+          data.end.toLocaleString(DateTime.DATETIME_FULL),
       );
       stateSwicher('day');
       setLocalStorage('day', data.begin, data.end);
     } else {
       console.log(
         'It is either before ' +
-          data.begin.format('MMMM Do YYYY, h:mm:ss a') +
+          data.begin.toLocaleString(DateTime.DATETIME_FULL) +
           ' or after ' +
-          data.end.format('MMMM Do YYYY, h:mm:ss a'),
+          data.end.toLocaleString(DateTime.DATETIME_FULL),
       );
 
       stateSwicher('night');
       setLocalStorage('night', data.begin, data.end);
     }
   });
-}
+};
 
-function setState() {
+const setState = () => {
   let cache;
+  const now = DateTime.local();
   try {
-    cache = JSON.parse(localStorage.getItem('state'));
+    // checks constant to see if it should cache
+    cache = ENABLE_CACHE ? JSON.parse(localStorage.getItem('state')) : null;
   } catch (err) {
     console.log('Cache parsing failed, manual calculations');
     calculateCorrectState();
@@ -111,18 +116,23 @@ function setState() {
   }
 
   if (cache == null) {
-    console.log('No cache, manual calculations');
+    if (ENABLE_CACHE) {
+      console.log('No cache, manual calculations');
+    } else {
+      console.log('ENABLE_CACHE is set to false, manual calculations');
+    }
+
     calculateCorrectState();
     return;
   }
 
-  if (moment.tz.guess() !== cache.tz) {
+  if (now.zoneName !== cache.tz) {
     console.log('Cache indicated changed timezone.');
     calculateCorrectState();
     return;
   }
 
-  if (moment().isBefore(cache.until)) {
+  if (now < cache.until) {
     console.log('Cache indicated state should not change.');
     stateSwicher(cache.state);
   } else {
@@ -132,9 +142,9 @@ function setState() {
     stateSwicher(cache.then);
     calculateCorrectState();
   }
-}
+};
 
-function stateSwicher(state) {
+const stateSwicher = state => {
   switch (state) {
     case 'day':
       makeDay();
@@ -147,28 +157,31 @@ function stateSwicher(state) {
       calculateCorrectState();
       break;
   }
-}
+};
 
-function setLocalStorage(state, startDayTime, endDayTime) {
+const setLocalStorage = (state, startDayTime, endDayTime) => {
+  if (!ENABLE_CACHE) return; // env variable
+
   let toStore = {};
-  toStore.tz = startDayTime.tz();
+  const now = DateTime.local();
+  toStore.tz = startDayTime.zoneName;
   if (state === 'day') {
     toStore.state = 'day';
-    toStore.until = endDayTime.format();
+    toStore.until = endDayTime.toISO();
     toStore.then = 'night';
   } else if (state === 'night') {
-    if (moment().isBefore(endDayTime)) {
+    if (now < endDayTime) {
       toStore.state = 'night';
-      toStore.until = startDayTime.format();
+      toStore.until = startDayTime.toISO();
       toStore.then = 'day';
     } else {
       toStore.state = 'night';
-      toStore.until = moment().endOf('day');
+      toStore.until = now.endOf('day');
       toStore.then = 'recalculate';
     }
   }
 
   localStorage.setItem('state', JSON.stringify(toStore));
-}
+};
 
 setState();
