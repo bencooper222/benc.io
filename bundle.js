9e9b8acc281aeac8c2bb148793faa5867da5b384
Benjamin Cooper <bencooper222@gmail.com>
2018-07-19 11:35:47 -0700

    Simplified with await

--- bundle.js
+++ bundle.js
@@ -9,45 +9,40 @@ const fs_writeFile = util.promisify(fs.writeFile);
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-(() => {
+(async () => {
   const bitly = BitlyClient(process.env.BITLY_API_KEY);
   const oldArticles = JSON.parse(
     fs.readFileSync('resources/articles.json', 'utf8')
   );
 
-  const minArticles = [];
-  const minReqs = [];
-  oldArticles.forEach(article => {
-    minReqs.push(
-      bitly
-        .shorten(article)
-        .then(minArticle => {
-          minArticles.push(
-            minArticle.data.url.substring(0, 5) === 'https'
-              ? minArticle.data.url
-              : `https${minArticle.data.url.substring(4)}`
-          );
-        })
-        .catch(err => {
-          console.log(err);
-          minArticles.push(article);
-        })
-    );
-  });
-  Promise.all(minReqs).then(() => {
-    fs_writeFile(
-      'resources/articles.use.json',
-      JSON.stringify(minArticles),
-      'utf8'
-    ).then(() => {
-      const bundler = new Parcel(path.join(__dirname, './index.html'), {
-        outDir: './build',
-        publicUrl: './',
-        watch: false,
-        minify: true,
-        scopeHoist: false
+  const minReqs = oldArticles.map(article => {
+    return bitly
+      .shorten(article)
+      .then(
+        minArticle =>
+          minArticle.data.url.substring(0, 5) === 'https'
+            ? minArticle.data.url
+            : `https${minArticle.data.url.substring(4)}`
+      )
+      .catch(err => {
+        console.error(err);
+        return article;
       });
-      bundler.bundle();
+  });
+
+  const minArticles = await Promise.all(minReqs);
+  fs_writeFile(
+    'resources/articles.use.json',
+    JSON.stringify(minArticles),
+    'utf8'
+  ).then(() => {
+    const bundler = new Parcel(path.join(__dirname, './index.html'), {
+      outDir: './build',
+      publicUrl: './',
+      watch: false,
+      minify: true,
+      scopeHoist: false
     });
+    bundler.bundle();
   });
 })();
