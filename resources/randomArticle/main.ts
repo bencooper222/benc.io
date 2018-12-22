28d5248d206e972bab95961df0bc1f217a7a9f1b
Benjamin Cooper <bencooper222@gmail.com>
2018-12-22 17:10:30 -0600

    Start rust

---
+++ resources/randomArticle/main.ts
@@ -0,0 +1,33 @@
+// import articleLinks from '../articles.use.json';
+
+// let articleLinksTemp: string[] = articleLinks.slice();
+// const link: HTMLLinkElement = document.getElementById(
+//   'link',
+// ) as HTMLLinkElement;
+// link.target = '_blank';
+
+// const randomPick = (initialLoad = false) => {
+//   if (articleLinksTemp.length === 0) {
+//     // if it's somehow empty, repopulate
+//     articleLinksTemp = articleLinks.slice();
+//   }
+
+//   // process to randomly select article
+//   const rand: number = Math.floor(Math.random() * articleLinksTemp.length);
+//   link.href = articleLinksTemp[rand];
+
+//   // add listener to change link onclick
+
+//   articleLinksTemp.splice(rand, 1); // remove from selection
+//   if (!initialLoad) {
+//     link.innerHTML = 'another reading';
+//   } // add animation in future?
+// };
+
+// randomPick(true);
+
+// link.addEventListener('click', () => {
+//   setTimeout(() => {
+//     randomPick();
+//   }, 11);
+// });
