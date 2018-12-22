ae9f48dd7c1292c90cbab7609f42cc939d0218ed
Benjamin Cooper <bencooper222@gmail.com>
2018-12-22 17:20:23 -0600

    Cleaned up bundling code

--- bundle.js
+++ bundle.js
@@ -1,5 +1,4 @@
 const Parcel = require('parcel-bundler');
-const BitlyClient = require('bitly');
 
 const path = require('path');
 const fs = require('fs');
@@ -10,30 +9,31 @@ const fs_writeFile = util.promisify(fs.writeFile);
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 (async () => {
-  const bitly = BitlyClient(process.env.BITLY_API_KEY);
+  const bitly = require('bitly')(process.env.BITLY_API_KEY);
   const oldArticles = JSON.parse(
     fs.readFileSync('resources/articles.json', 'utf8'),
   );
 
-  const minReqs = oldArticles.map(article => {
-    return bitly
-      .shorten(article)
-      .then(
-        minArticle =>
-          minArticle.data.url.substring(0, 5) === 'https'
-            ? minArticle.data.url
-            : `https${minArticle.data.url.substring(4)}`,
-      )
-      .catch(err => {
-        console.error(err);
-        return article;
-      });
-  });
-
-  const minArticles = await Promise.all(minReqs);
   fs_writeFile(
     'resources/articles.use.json',
-    JSON.stringify(minArticles),
+    JSON.stringify(
+      await Promise.all(
+        oldArticles.map(article => {
+          return bitly
+            .shorten(article)
+            .then(
+              minArticle =>
+                minArticle.data.url.substring(0, 5) === 'https'
+                  ? minArticle.data.url
+                  : `https${minArticle.data.url.substring(4)}`,
+            )
+            .catch(err => {
+              console.error(err);
+              return article;
+            });
+        }),
+      ),
+    ),
     'utf8',
   ).then(() => {
     const bundler = new Parcel(path.join(__dirname, './index.html'), {
